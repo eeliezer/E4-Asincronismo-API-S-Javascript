43ff385abb50d43cb278d43287e226dbee07606d
eeliezer <eliezer_job@hotmail.com>
2022-11-08 17:01:10 -0300

    Fix logica

--- assets/main.js
+++ assets/main.js
@@ -20,11 +20,11 @@ const renderPokemon = (pokemon) => {
     cajaPoke.innerHTML =  
         `
         <div id="pokeResultado">
-            <h1>El número de pokemon: ${id}</h1>
-            <p>Nombre pokemon: ${name.charAt(0).toUpperCase() + name.slice(1)}</p>
-            <p>Tipo: ${types.map( tipo => { return `<span>${tipo.type.name}</span>` } ).join(' ')}</p>
-            <p>Peso: ${weight / 10}</p>
-            <p>Peso: ${height / 10}</p>
+            <h1>El número de Pokémon: ${id}</h1>
+            <p>Nombre Pokémon: ${name.charAt(0).toUpperCase() + name.slice(1)}</p>
+            <p>Tipo: ${types.map( tipo => { return `<img class="pokeType" src="./assets/img/${tipo.type.name}.png" alt="${tipo.type.name}"><span>${tipo.type.name.charAt(0).toUpperCase() + tipo.type.name.slice(1)}</span>` } ).join(' ')}</p>
+            <p>Peso: ${weight / 10}kg</p>
+            <p>Peso: ${height / 10}mts</p>
             <img class="imgPoke" src="${sprites.other.home.front_default}" alt="poke">
         </div>
         `
@@ -34,7 +34,7 @@ const renderError = (id) => {
     cajaPoke.innerHTML =
         `
         <div>
-            <h1>El numero de pokemon: ${id} no existe</h1>
+            <h1>El numero de pokémon: ${id} No existe</h1>
             <img class="sadPika" src="./assets/img/sad_pikachu.gif">
         </div>
         `
@@ -61,7 +61,7 @@ const searchPoke = async (e) => {
         return renderError(pokeIdIngresado); //alert("No existe")
     } else if (pokeIdIngresado === fetchedPokes.id) {
         form.reset();
-        return alert("Ya estamos mostrando el pokemon")
+        return alert("Ya estamos mostrando el Pokémon")
     }
     // si paso la validacion, lo agrego a mi lista de pokemones
     loader.classList.remove('hidden')
